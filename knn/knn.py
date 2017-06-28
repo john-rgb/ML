66b4b50f92aabc4ad27be07a4c4615ed1191e772
lcy1 <947822561@qq.com>
2017-06-28 21:52:42 +0800

    170628
    添加中文注释，重新理解

--- knn/knn.py
+++ knn/knn.py
@@ -13,24 +13,33 @@ def creatDataSet():
     labels=['A','A','B','B']
     return group,labels
 def kNNClassify(newInput, dataSet, labels, k):  
-    numSamples = dataSet.shape[0] # shape[0] stands for the num of row  
-  
+    numSamples = dataSet.shape[0]# shape[0] stands for the num of row样本的个数
+    #print(numSamples)
     ## step 1: calculate Euclidean distance  
     # tile(A, reps): Construct an array by repeating A reps times  
     # the following copy numSamples rows for dataSet  
-    diff = tile(newInput, (numSamples, 1)) - dataSet # Subtract element-wise  
-    squaredDiff = diff ** 2 # squared for the subtract  
-    squaredDist = sum(squaredDiff, axis = 1) # sum is performed by row  
-    distance = squaredDist ** 0.5  
+    diff = tile(newInput, (numSamples, 1)) - dataSet # Subtract element-wise求差值
+    # print(diff)
+    squaredDiff = diff ** 2 # squared for the subtract求x的平方和y的平方
+    # print(squaredDiff)
+    squaredDist = sum(squaredDiff, axis = 1) # sum is performed by row平方和
+    # print(squaredDist)
+    distance = squaredDist ** 0.5  #平方和开方也就是距离
+    # print(distance)
   
     ## step 2: sort the distance  
-    # argsort() returns the indices that would sort an array in a ascending order  
-    sortedDistIndices = argsort(distance)  
+    # argsort() returns the indices that would sort an array in a ascending order
+    print(distance)
+    sortedDistIndices = argsort(distance)
+    print(sortedDistIndices)
   
     classCount = {} # define a dictionary (can be append element)  
     for i in range(k):  
-        ## step 3: choose the min k distance  
-        voteLabel = labels[sortedDistIndices[i]]  
+        ## step 3: choose the min k distance
+        print(i)
+        print(sortedDistIndices[i])
+        voteLabel = labels[sortedDistIndices[i]]
+        print(voteLabel)
   
         ## step 4: count the times labels occur  
         # when the key voteLabel is not in dictionary classCount, get()  
--- knn/test.py
+++ knn/test.py
@@ -6,22 +6,16 @@ Created on Wed Mar  1 15:32:32 2017
 """
 
 import knn
-#from numpy import *
+from numpy import *
 
-group,labels=knn.creatDataSet()
+dataSet, labels = knn.creatDataSet()
 
-#print(group)
-#print(labels)
-def classify(inX,dataSet,labels,k):
-    dataSetSize=dataSet.shape[0]
-    diffMat=tile(inX,(dataSetSize,1))-dataSet
-    sqDiffMat=diffMat**2
-    sqDistanses=sqDiffMat.sum(axis=1)
-    distenses=sqDistanses**0.5
-    sortedDistenses=distenses.argsort()
-    classCount={}
-    for i in range(k):
-        voteIlabel=labels[sortedDistenses[i]]
-        classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
-    sortedClassCount=sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
-    return sortedClassCount[0][0]
+testX = array([0.5, 0.5])
+k = 3
+outputLabel = knn.kNNClassify(testX, dataSet, labels, 3)
+print("Your input is:", testX, "and classified to class: ", outputLabel)
+
+
+testX = array([0.1, 0.3])
+outputLabel = knn.kNNClassify(testX, dataSet, labels, 3)
+print("Your input is:", testX, "and classified to class: ", outputLabel)
